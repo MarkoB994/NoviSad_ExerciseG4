ea7da46249e81e6f7e07e305222b99148f0cd714
somename <someemail>
2017-06-09 20:19:23 +0200

    besouro automatic message

--- tests/TestBowling.java
+++ tests/TestBowling.java
@@ -12,27 +12,32 @@ public class TestBowling {
 	}
 	
 	@Test 
-	public void test_PoeniPrvogFrejma(){
+	public void test_PoeniPrvogFrejma() throws BowlingException{
 		Frame frame = new Frame(6,3);
 		String rezultat = frame.getScore();
 		assertEquals("Poeni ostvareni u prvom frejmu", "9", rezultat);
 	}
 	
 	@Test
-	public void test_ProveraStrajkaFrejma(){
+	public void test_ProveraStrajkaFrejma() throws BowlingException{
 		Frame frame = new Frame(10,0);
 		assertTrue("Provera strajka", frame.isStrike());
 	}
 	
 	@Test
-	public void test_ProveraSperaFrejma(){
+	public void test_ProveraSperaFrejma() throws BowlingException{
 		Frame frame = new Frame(6,4);
 		assertTrue("Provera spera", frame.isSpare());
 	}
 	
 	@Test(expected = BowlingException.class)
-	public void test_ProveraPogresnihVrednostiPrvogBacanjaUFrejmu(){
+	public void test_ProveraPogresnihVrednostiPrvogBacanjaUFrejmu() throws BowlingException{
 		Frame frame = new Frame(11,5);
+	}
+	
+	@Test(expected = BowlingException.class)
+	public void test_ProveraPogresnihVrednostiDrugogBacanjaUFrejmu() throws BowlingException{
+		Frame frame = new Frame(7,5);
 	}
 
 }
