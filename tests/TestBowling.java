8b892cecf190097d3b0534d3494e962ec2749ef9
somename <someemail>
2017-06-09 19:57:23 +0200

    besouro automatic message

--- tests/TestBowling.java
+++ tests/TestBowling.java
@@ -5,8 +5,10 @@ import org.junit.Test;
 public class TestBowling {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void test_PocetakPartije() {
+		Frame frame = new Frame();
+		String rezultat =  frame.getScore();
+		assertEquals("Rezultat prvog frejma na samom pocetku", "0", rezultat);
 	}
 
 }
