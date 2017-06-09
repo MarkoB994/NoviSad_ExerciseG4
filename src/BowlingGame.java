714b4b84710f26ecc89495e8a92854b24659cf21
somename <someemail>
2017-06-09 20:31:17 +0200

    besouro automatic message

--- src/BowlingGame.java
+++ src/BowlingGame.java
@@ -14,7 +14,7 @@ public class BowlingGame {
 	
 	// adds a frame to the game
 	public void addFrame(Frame frame){
-		//to be implemented
+		frames.add(frame);
 	}
 	
 	// Sets the bonus throws at the end of the game
@@ -26,5 +26,9 @@ public class BowlingGame {
 	public int score(){
 		//to be implemented: should return game score 
 		return 0;
+	}
+	
+	public String getNumberOfFrames(){
+		return frames.size() + "";
 	}
 }
