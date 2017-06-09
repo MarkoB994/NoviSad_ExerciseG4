1ea00b6a24ea8d14d12ad0095cd306625a95ab25
somename <someemail>
2017-06-09 20:54:50 +0200

    besouro automatic message

--- src/BowlingGame.java
+++ src/BowlingGame.java
@@ -9,7 +9,7 @@ public class BowlingGame {
 	//a bowling game is made of (at least) 10 frames
 	private List<Frame> frames = new ArrayList<Frame>();
 	private Frame bonus;
-	private boolean bonusZaSpare;
+	private boolean bonusZaSpare, bonusZaStrike;
 	private int rezultat = 0;
 	
 	public BowlingGame(){}
@@ -26,9 +26,17 @@ public class BowlingGame {
 	public void setBonus(int firstThrow, int secondThrow) {
 		if(bonusZaSpare){
 			rezultat += firstThrow;
+			bonusZaSpare = false;
+		}
+		if(bonusZaStrike){
+			rezultat += (firstThrow + secondThrow);
+			bonusZaStrike = false;
 		}
 		if(firstThrow + secondThrow == 10 && firstThrow != 10){
 			bonusZaSpare = true;
+		}
+		if(firstThrow == 10){
+			bonusZaStrike = true;
 		}
 		rezultat += (firstThrow + secondThrow);
 	}
